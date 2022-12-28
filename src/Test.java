6c6f1e5eb06cff862b918771be63973f6b59a095
bani92 <banseok9204@naver.com>
2022-12-28 22:58:46 +0900

    ex1_1

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,13 @@
 public class Test {
+    public static void main(String[] args) {
+
+        int num1 = 4;
+        int num2 = 2;
+        int num3;
+        num3 = num1%num2;
+        System.out.println(num3);
+
+
+
+    }
 }
