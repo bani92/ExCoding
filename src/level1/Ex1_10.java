66257a375e211961a4b8d5c0ee3b423993cffa1b
bani92 <banseok9204@naver.com>
2023-01-13 14:04:39 +0900

    Add0113

---
+++ src/level1/Ex1_10.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class Ex1_10 {
+    public static void main(String[] args) {
+        String[] seoul = {"jane","kim","jain"}
+        String answer = "";
+        int x = 0;
+
+
+        for(int i=0; i<seoul.length;i++) {
+            if(seoul[i].equals("kim")) {
+                x = i;
+            }
+        }
+        answer = "김서방은 " + x + "에 있다";
+        System.out.println(answer);
+    }
+
+}
