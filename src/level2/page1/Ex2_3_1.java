1dd55ade662550d0c2868863a747d6708cf5d479
bani92 <banseok9204@naver.com>
2023-01-21 23:45:27 +0900

    Add0121

--- src/level2/page1/Ex2_3_1.java
+++ src/level2/page1/Ex2_3_1.java
@@ -2,11 +2,11 @@ package level2.page1;
 
 public class Ex2_3_1 {
     public static void main(String[] args) {
-        String s = "aaaaa     aaa ";
+        String s = "aaaaa     aaa   ";
         s = s.toLowerCase();
         String answer = "";
         System.out.println(s);
-        String a[] = new String[s.length()];
+        String[] a = new String[s.length()];
 
         // String 배열로 만들고 배열에 한 글자씩 담기
         // 문자
@@ -14,14 +14,19 @@ public class Ex2_3_1 {
             a[i] = String.valueOf(s.charAt(i));
 
         }
-        System.out.println(s.length());
+
+
         for(int i=0; i<s.length() ; i++) {
-            if(a[i].equals(" ")) {
-                a[i+1] = a[i+1].toUpperCase();
+            // 자리가 빈칸이고 i가 마지막숫자가 아니면 대문자로
+            // 자리가 빈칸이고 i가 마지막숫자이면 break;
+            if(a[i].equals(" "))
+            {
+                if(i!=s.length()-1) {
+                    a[i+1] = a[i+1].toUpperCase();
+                }
+
             } else if(s.charAt(0) >= 'a' && s.charAt(0) <= 'z') {
                 a[0] = a[0].toUpperCase();
-            } else if(a[i].equals(" ") && a[s.length()].equals(" ")) {
-
             }
 
         }
