2087527d474d4674c08fd0d0360f1ea50179da99
bani92 <banseok9204@naver.com>
2023-01-23 23:10:19 +0900

    Add0123

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,24 @@
 public class Test {
     public static void main(String[] args) {
 
-        int num1 = 4;
-        int num2 = 2;
-        int num3;
-        num3 = num1%num2;
-        System.out.println(num3);
+        int list_size = 5; // 배열 사이즈
+        int[] list = new int[list_size]; // 배열의 크기가 5인 배열을 생성
+        int count = 0; // 배열 원소가 이동할 때 마다 count
+
+        for(int j = 0; j < 5; j++) {
+            list[j] = j+1; // 인덱스가 i일 때 배열 값은 i+1을 넣는다.
+        }
+
+        for(int i = 1; i < list.length-1; i++) { // 우리가 삭제하고 싶은 인덱스 값부터 마지막 인덱스까지 반복문을 돌린다.
+            list[i] = list[i+1];  // 인덱스 i+1의 값을 인덱스 i로 이동
+            count++;  // 이동할 때마다 count 증가
+        }
+
+        for(int j = 0; j < 5; j++) {
+            System.out.print(list[j]+" ");    }
+
+//        System.out.println(count); // counting을 확인한다.
+//        System.out.println(list[1]); // 우리가 원하는 배열 값을 확인한다.
 
 
 
--- src/level1/Ex1_13.java
+++ src/level1/Ex1_13.java
@@ -6,48 +6,42 @@ import java.util.List;
 
 public class Ex1_13 {
     public static void main(String[] args) {
-        int[] arr = {4,3,2,1};
+        int[] arr = {4,10,1,5,3,2};
         int[] answer = {};
 
+       int min = arr[0];
 
+       if(arr.length==1) {
+           answer = new int[1];
+           answer[0] = -1;
+       } else {
+           answer = new int[arr.length-1];
+       }
+        for(int i=0; i<arr.length ; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        int j = 0;
         for(int i=0; i<arr.length ; i++) {
 
+            if(arr[i]!=min) {
+              answer[j] = arr[i];
+              j++;
+
+            }
 
         }
-
-        if(arr.length==1) {
-            arr[0] = -1;
+        for(int i=0; i<answer.length ; i++) {
+            System.out.print(answer[i]);
         }
 
 
 
-//        // min보다 arr[i] 크면
-//        // arr[i] 값이 min으로 들어감
-//        // 작은수자는 계속 min에 들어가있음
-//        for(int i=0; i<arr.length ; i++) {
-//            if(min>arr[i]) {
-//                min = arr[i];
-//
-//            } else {
-//                b++; // 배열의 크기때문에
-//
-//            }
-//
-//        }
-//        System.out.println(b);
-//        answer = new int[b];
-//        if(b==1) {
-//            answer[0] = -1;
-//        }
-//        for(int i=0, j=0; i<arr.length ; i++) {
-//            if(min!=arr[i]) {
-//                answer[j] = arr[i];
-//                j++;
-//            }
-//        }
-//
-//        for(int i=0; i<answer.length; i++) {
-//            System.out.println(answer[i]);
-//        }
+
+
+
+
+
     }
 }
