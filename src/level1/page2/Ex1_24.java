332e7796d1ecc586ef05adb0bc94295ddae9ddbe
bani92 <banseok9204@naver.com>
2023-02-05 23:28:01 +0900

    Add0205

--- src/level1/page2/Ex1_24.java
+++ src/level1/page2/Ex1_24.java
@@ -3,14 +3,27 @@ package level1.page2;
 public class Ex1_24 {
     public static void main(String[] args) {
 
-        int a = 5;
-        int b = 3;
+       int n = 13;
+       int m = 17;
 
-        for(int i=0; i<b ; i++) {
-            for(int j=0; j<a ; j++) {
-                System.out.print("*");
+       int answer = 0;
+        // 13 14 15 -16 17
+        for(int j=n ; j<=m ; j++) {
+            int a = 0;
+            for(int i=1; i<=j ; i++) {
+                if(j%i==0) {
+                    a++;
+                }
             }
-            System.out.println();
+
+            if(a%2==0) {
+                answer += j;
+            } else {
+                answer -= j;
+            }
+            System.out.println(answer);
         }
+
+
     }
 }
