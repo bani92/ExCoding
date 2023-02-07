c315b78147eb83cf7f4479e0f783895af26b7fb8
bani92 <banseok9204@naver.com>
2023-02-07 10:23:35 +0900

    Add0207

--- src/level1/page2/Ex1_25.java
+++ src/level1/page2/Ex1_25.java
@@ -21,19 +21,33 @@ public class Ex1_25 {
 //            answer[0] = 1;
 //            answer[1] = n*m;
 //        }
+        // 3 12
+        int x = 0;
+        if(n<m) {
+            for(int i=1; i<=n;i++) {
+                if(n%i==0 && m%i==0) {
+                    x = i;
+                }
 
-        for(int i=1; i<=n;i++) {
-           if(n%i==0) {
-               System.out.println(i);
-           }
-        }
+            }
 
-        System.out.println();
-        for(int j=1; j<=m;j++) {
-            if(m%j==0) {
-                System.out.println(j);
+        } else {
+            for(int i=1; i<=m;i++) {
+                if(n%i==0 && m%i==0) {
+                    x = i;
+                }
+
             }
         }
+        answer[0] = x;
+        answer[1] = n/x * m/x * x;
+        System.out.println(answer[1]);
+//        System.out.println();
+//        for(int j=1; j<=m;j++) {
+//            if(m%j==0) {
+//                System.out.println(j);
+//            }
+//        }
 //        System.out.println(answer[0]);
 //        System.out.println(answer[1]);
     }
