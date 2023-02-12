1fdc1b7fd8bd47a3231733174b12c92ee5f1225f
bani92 <banseok9204@naver.com>
2023-02-12 22:39:45 +0900

    Add0212

--- src/level1/page2/Ex1_27.java
+++ src/level1/page2/Ex1_27.java
@@ -1,11 +1,37 @@
 package level1.page2;
 
+import java.util.ArrayList;
+
 public class Ex1_27 {
     public static void main(String[] args) {
-        String[] s = {"try hello world"};
 
-        for(int i=0; i<s.length ; i++) {
+        int n = 125;
+        int answer = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; ; i++) {
+             list.add(n % 3);
+           n = n / 3;
+            System.out.println(list);
+           if(n==0) {
+               break;
+           }
 
         }
+
+        for(int j=list.size()-1, z=0; j>=0 ; j--) {
+            int i = z == 0 ? list.get(j) : (int) (list.get(j) * Math.pow(3, z));
+
+            answer += i;
+            z++;
+            System.out.println(answer);
+
+        }
+//        while(n>=0) {
+//            n /= 3;
+//            if(n==0) {
+//                break;
+//            }
+//            System.out.println(n);
+//        }
     }
 }
