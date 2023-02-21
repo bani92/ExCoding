7dbe3552f875dfe616786891a8f9ebbdc7aa980d
bani92 <banseok9204@naver.com>
2023-02-21 16:44:16 +0900

    Add0221 진행중

--- src/level1/page2/Ex1_33.java
+++ src/level1/page2/Ex1_33.java
@@ -1,4 +1,26 @@
 package level1.page2;
 
 public class Ex1_33 {
+    public static void main(String[] args) {
+
+        int n = 5;
+        int[] arr1 = {20,20,28,18,11};
+        int[] arr2 = {30,1,21,17,28};
+        String s = "";
+        for(int i=0 ;i<1 ; i++) {
+
+
+            while(arr1[i]>=1) {
+                System.out.print(arr1[i] + " ");
+                s += arr1[i]%2;
+                arr1[i] = arr1[i]/2;
+                System.out.println(s);
+//                System.out.println(arr1[i]);
+            }
+            System.out.println();
+
+        }
+
+
+    }
 }
