70faaf15e8313db44d32b406e583b72d5ffcca3b
bani92 <banseok9204@naver.com>
2023-02-22 22:49:30 +0900

    Add0222 완료

--- src/Test.java
+++ src/Test.java
@@ -1,26 +1,50 @@
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
 
-        int list_size = 5; // 배열 사이즈
-        int[] list = new int[list_size]; // 배열의 크기가 5인 배열을 생성
-        int count = 0; // 배열 원소가 이동할 때 마다 count
+       ArrayList<Ex001> arrayist = new ArrayList<Ex001>();
 
-        for(int j = 0; j < 5; j++) {
-            list[j] = j+1; // 인덱스가 i일 때 배열 값은 i+1을 넣는다.
-        }
+       Ex001 ex01 = new Ex001();
+       ex01.setA("a");
+       ex01.setB(2);
+       ex01.setC(5);
+       arrayist.add(ex01);
 
-        for(int i = 1; i < list.length-1; i++) { // 우리가 삭제하고 싶은 인덱스 값부터 마지막 인덱스까지 반복문을 돌린다.
-            list[i] = list[i+1];  // 인덱스 i+1의 값을 인덱스 i로 이동
-            count++;  // 이동할 때마다 count 증가
-        }
-
-        for(int j = 0; j < 5; j++) {
-            System.out.print(list[j]+" ");    }
-
-//        System.out.println(count); // counting을 확인한다.
-//        System.out.println(list[1]); // 우리가 원하는 배열 값을 확인한다.
+       for(Ex001 s : arrayist) {
+           System.out.println(s.getA());
+           System.out.println(s.getB());
+           System.out.println(s.getC());
+       }
 
 
 
+
+    }
+}
+
+class Ex001 {
+
+    private String a;
+    private int b;
+    private int c;
+
+    public String getA() {
+        return a;
+    }
+    public void setA(String a) {
+        this.a = a;
+    }
+    public int getB() {
+        return b;
+    }
+    public void setB(int b) {
+        this.b = b;
+    }
+    public int getC() {
+        return c;
+    }
+    public void setC(int c) {
+        this.c = c;
     }
 }
--- src/level1/page2/Ex1_33.java
+++ src/level1/page2/Ex1_33.java
@@ -3,24 +3,67 @@ package level1.page2;
 public class Ex1_33 {
     public static void main(String[] args) {
 
-        int n = 5;
-        int[] arr1 = {20,20,28,18,11};
-        int[] arr2 = {30,1,21,17,28};
-        String s = "";
-        for(int i=0 ;i<1 ; i++) {
+        int n = 6;
+        int[] arr1 = {46,33,33,21,31,50};
+        int[] arr2 = {27,56,19,14,14,10};
+//        String[] answer = new String[arr1.length];
+//        String[] answer1 = new String[arr2.length];
+
+        String[] answer = new String[arr1.length];
+        String[] answer1 = new String[arr2.length];
+
+        String[] result;
+        result = new String[arr1.length];
 
 
+        // 10진수 -> 2진수 변환
+        for(int i=0 ;i<arr1.length ; i++) {
+            String s = "";
+            String s1 = "";
             while(arr1[i]>=1) {
-                System.out.print(arr1[i] + " ");
+
                 s += arr1[i]%2;
                 arr1[i] = arr1[i]/2;
-                System.out.println(s);
-//                System.out.println(arr1[i]);
             }
-            System.out.println();
+            while(arr2[i]>=1) {
+                s1 += arr2[i]%2;
+                arr2[i] = arr2[i]/2;
+            }
+        StringBuffer sb = new StringBuffer(s);
+        StringBuffer sb1 = new StringBuffer(s1);
+        // 모자란 자릿수 0으로 채우기
+            if(sb.length()<n) {
+                while(sb.length()<n) {
+                    sb.append(0);
+                }
+            }
+            if(sb1.length()<n) {
+                while(sb1.length()<n) {
+                    sb1.append(0);
+                }
+            }
+            answer[i] = sb.reverse().toString();
+            answer1[i] = sb1.reverse().toString();
 
         }
+        // 치환하기
+        for(int k=0; k<result.length ;k++) {
+            result[k] = "";
+            for(int j=0; j<answer[k].length(); j++) {
+                int s2 =(answer[k].charAt(j) | answer1[k].charAt(j));
+                System.out.println(s2);
+                if(s2 == 49) {
+                    result[k] += "#";
 
+                } else {
+                    result[k] += " ";
+                }
+            }
+            System.out.println();
+        }
+        for(int z=0 ; z<result.length ; z++) {
+            System.out.println(result[z]);
+        }
 
     }
 }
