555913a3824a04be03e2d4e7a66cc97cfb7ba17c
bani92 <banseok9204@naver.com>
2023-03-02 10:49:17 +0900

    0302 K번째수 완료

--- src/level1/page3/Ex1_38.java
+++ src/level1/page3/Ex1_38.java
@@ -6,9 +6,16 @@ public class Ex1_38 {
     public static void main(String[] args) {
         int[] array = {1,5,2,6,3,7,4};
         int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+        int[] answer = {};
+        answer = new int[commands.length];
+
+        for(int i=0; i< commands.length ; i++) {
+            int[] array2 = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(array2);
+            answer[i] = array2[commands[i][2]-1];
 
 
-        int[] array2 = Arrays.copyOfRange(array,1,5);
-        System.out.println(Arrays.toString(array2));
+        }
+
     }
 }
