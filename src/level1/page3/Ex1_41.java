c00be71cd2fcb001cb461170b1ada36ce7af77aa
bani92 <banseok9204@naver.com>
2023-03-07 22:35:41 +0900

    0307 콜라 문제 진행중

--- src/level1/page3/Ex1_41.java
+++ src/level1/page3/Ex1_41.java
@@ -2,6 +2,19 @@ package level1.page3;
 
 public class Ex1_41 {
     public static void main(String[] args) {
+        int a = 2;
+        int b = 1;
+        int n = 20;
+        int answer = 0;
 
+
+        while(n > a) {
+            answer += n/a;
+          n =  n - (a * (n/a)) + n/a;
+            System.out.println(n);
+
+        }
+
+       // System.out.println(answer);
     }
 }
